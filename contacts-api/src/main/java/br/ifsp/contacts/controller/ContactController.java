8015ca06adbadd9d4c7126290394a25a71fd6fa5
Aluno <giulliameneses@gmail.com>
2026-03-19 18:54:46 -0300

    Começando classe controller, metodos GET E POST

---
+++ contacts-api/src/main/java/br/ifsp/contacts/controller/ContactController.java
@@ -0,0 +1,39 @@
+package br.ifsp.contacts.controller;
+
+import br.ifsp.contacts.model.Contact;
+import br.ifsp.contacts.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/*
+* Classe responsavel por mapear as rotas ou endpoints dos contatos
+* */
+
+@RestController //indica que é um controller de requisições REST
+@RequestMapping("/api/contacts")// indica o caminhos dos endpoints
+
+public class ContactController {
+
+    @Autowired //injeta automaticamente uma instancia
+    private ContactRepository contactRepository; //classe repository
+
+    @GetMapping//indica que o endpoint chaqmado é metodo GET
+    public List<Contact> getAllContacts(){
+        return contactRepository.findAll();
+    }
+
+    @GetMapping("/{id}")//indica metodo GET
+    public Contact getContactId(@PathVariable Long id){ //@PathVariable "amarra" a variável {id} da URL
+        // findById retorna um Optional, então usamos orElseThrow
+        return contactRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Contato não encontrado!"))
+    }
+
+    @PostMapping//indica metodo POST (create)
+    public Contact createContact(@RequestBody Contact contact){ //@RequestBody indica que o objeto Contact será preenchido
+        return contactRepository.save(contact);
+    }
+
+}
